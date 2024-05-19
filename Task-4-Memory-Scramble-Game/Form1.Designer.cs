1418db4476d2c07bdf73262753dd96091d5beb5e
mahmoud097 <74962993+mahmoud097@users.noreply.github.com>
2024-05-19 23:17:59 +0300

    Add files via upload
    #adding button, labels and list 
    making timer from 30 sec 
    create variables

--- Task-4-Memory-Scramble-Game/Form1.Designer.cs
+++ Task-4-Memory-Scramble-Game/Form1.Designer.cs
@@ -28,18 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            lblTextLeft = new Label();
+            btnRestart = new Button();
+            GameTimer = new System.Windows.Forms.Timer(components);
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(334, 103);
+            label1.Name = "label1";
+            label1.Size = new Size(136, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Match or MisMatch";
+            // 
+            // lblTextLeft
+            // 
+            lblTextLeft.AutoSize = true;
+            lblTextLeft.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTextLeft.Location = new Point(327, 162);
+            lblTextLeft.Name = "lblTextLeft";
+            lblTextLeft.Size = new Size(143, 28);
+            lblTextLeft.TabIndex = 1;
+            lblTextLeft.Text = "Time Left : 30";
+            // 
+            // btnRestart
+            // 
+            btnRestart.BackColor = SystemColors.InactiveBorder;
+            btnRestart.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnRestart.Location = new Point(327, 26);
+            btnRestart.Name = "btnRestart";
+            btnRestart.Size = new Size(143, 49);
+            btnRestart.TabIndex = 2;
+            btnRestart.Text = "Restart";
+            btnRestart.UseVisualStyleBackColor = false;
+            btnRestart.Click += RestartGameEvent;
+            // 
+            // GameTimer
+            // 
+            GameTimer.Interval = 1000;
+            GameTimer.Tick += TimerEvent;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
             ClientSize = new Size(506, 367);
+            Controls.Add(btnRestart);
+            Controls.Add(lblTextLeft);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "Memory-Scramble-Game";
+            Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label lblTextLeft;
+        private Button btnRestart;
+        private System.Windows.Forms.Timer GameTimer;
     }
 }
--- Task-4-Memory-Scramble-Game/Form1.cs
+++ Task-4-Memory-Scramble-Game/Form1.cs
@@ -2,9 +2,89 @@ namespace Task_4_Memory_Scramble_Game
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        //initiate List numbers
+        List<int> numbers = new List<int> { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6 };
+        string firstChoice;
+        string secondChoice;
+        int tries;
+        List<PictureBox> pictures = new List<PictureBox>();
+        PictureBox picA;
+        PictureBox picB;
+        int totalTime = 30;
+        int countDownTime;
+        bool gameOver = false;
+
+        public Form1()          // call method load pics
         {
             InitializeComponent();
+            LoadPictures();
+        }
+
+        private void TimerEvent(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RestartGameEvent(object sender, EventArgs e)
+        {
+
+        }
+        private void LoadPictures()
+        {
+            int leftPos = 20;
+            int topPos = 20;
+            int rows = 0;
+
+            for (int i = 0; i < 12; i++)
+            {
+                PictureBox newPic = new PictureBox();
+                newPic.Height = 50;
+                newPic.Width = 50;
+                newPic.BackColor = Color.LightGray;
+                newPic.SizeMode = PictureBoxSizeMode.StretchImage;
+                newPic.Click += NewPic_Click;
+                pictures.Add(newPic);
+
+                if (rows < 3)
+                {
+                    rows++;
+                    newPic.Left = leftPos;
+                    newPic.Top = topPos;
+                    this.Controls.Add(newPic);
+                    leftPos = leftPos + 60;
+                }
+
+                if (rows == 3)
+                {
+                    leftPos = 20;
+                    topPos += 60;
+                    rows = 0;
+                }
+            }
+            RestartGame();
+        }
+
+        private void NewPic_Click(object? sender, EventArgs e)
+        {
+
+        }
+
+        private void RestartGame()
+        {
+
+        }
+        private void CheckPictures(PictureBox A, PictureBox B)
+        {
+
+        }
+        private void GameOver()
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
